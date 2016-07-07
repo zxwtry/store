4b6aa1c7153fdb566e5067d5b81149e995287c0c
zxwtry <zxwtry@qq.com>
2016-07-07 15:26:17 +0800

    添加hosts

---
+++ shell/201607/S004_hosts.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "192.168.160.150   centos0" >> /etc/hosts
+echo "192.168.160.151   centos1" >> /etc/hosts
+echo "192.168.160.152   centos2" >> /etc/hosts
+echo "192.168.160.153   centos3" >> /etc/hosts
--- shell/readme.md
+++ shell/readme.md
@@ -1,3 +1,4 @@
 S001    201606  hadoop相关操作的shell<br/>
 S002    201606  shell学习<br/>
 S003    201607  ZooKeeper的启动和关闭<br/>
+S004    201607  添加hosts<br/>
