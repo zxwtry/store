e3c079b99278e2231677617edf1c9bf50c2ceaea
zxwtry <zxwtry@qq.com>
2016-07-07 15:36:39 +0800

    添加scp

--- shell/201607/S004_hosts.sh
+++ shell/201607/S004_hosts.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+#encoding=utf-8
 echo "192.168.160.150   centos0" >> /etc/hosts
 echo "192.168.160.151   centos1" >> /etc/hosts
 echo "192.168.160.152   centos2" >> /etc/hosts
 echo "192.168.160.153   centos3" >> /etc/hosts
+#第二个文件
+#!/bin/sh
+scp S004_hosts.sh root@centos1:/root
+scp S004_hosts.sh root@centos2:/root
+scp S004_hosts.sh root@centos3:/root
