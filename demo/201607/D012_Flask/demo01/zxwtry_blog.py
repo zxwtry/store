a35fc9590bf95f8640a5e98c2f9f3a4019922849
zxwtry <zxwtry@qq.com>
2016-08-05 14:09:44 +0800

    添加D012 blog的数据库

--- demo/201607/D012_Flask/demo01/zxwtry_blog.py
+++ demo/201607/D012_Flask/demo01/zxwtry_blog.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python
 #encoding=utf-8
 
-from flask import Flask, request
+from __future__ import with_statement
+from contextlib import closing
+from flask import Flask, request, session, url_for, g,\
+		 render_template, redirect, abort, flash
 
 app=Flask(__name__)
+app.config.from_object(__name__)
+app.config.from_envvar("FLASKR_SETTINGS", silent=True)
 
 
+if __name__=='__main__':
+	app.run(host='0.0.0.0')
--- demo/201607/D012_Flask/demo01/zxwtry_blog_mysql.py
+++ demo/201607/D012_Flask/demo01/zxwtry_blog_mysql.py
@@ -7,16 +7,7 @@ def insert(title, text):
 	try:
 		conn=sql.connect(host='localhost', user='root', passwd='12356', port=3306, charset='utf8')
 		cur=conn.cursor()
-		#cur.execute("drop database flask if exists;")
-		#cur.execute('create database flask')
 		conn.select_db('flask')
-		#cur.execute('drop table entries if exists;')
-		#cur.execute('create table entries(id int primary key auto_increment, title varchar(60) not null, text varchar(60) not null) default charset=utf8;')
-		#title=str(title).encode("utf-8")
-		#text=str(text).encode("utf-8")
-		#title=unicode(title)
-		#text=unicode(text)
-		#cur.execute('insert into entries(title, text) values("'+title+'", "'+text+'")')
 		cur.execute('insert into entries(title, text) values("%s", "%s")' %(title, text))
 		conn.commit()
 		cur.close()
@@ -28,10 +19,29 @@ def drop_create_database():
 	try:
 		conn=sql.connect(host='localhost', user='root', passwd='12356', port=3306, charset='utf8')
 		cur=conn.cursor()
-		con.execute('drop database flask if exists database flask')
+		cur.execute('drop database if exists flask')
+		cur.execute('create database flask')
+		conn.commit()
+		cur.close()
+		conn.close()
 	except sql.Error, e:
-		print "MySQL error %d: %s" %
+		print "MySQL error %d: %s" %(e.args[0], e.args[1])
+
+def drop_create_table():
+	try:
+		conn=sql.connect(host='localhost', user='root', passwd='12356', port=3306, charset='utf8')
+		cur=conn.cursor()
+		conn.select_db('flask')
+		cur.execute('drop table if exists entries');
+		cur.execute('create table entries(id int primary key auto_increment, title varchar(60) not null, text varchar(60) not null) default charset=utf8')
+		conn.commit()
+		cur.close()
+		conn.close()
+	except sql.Error, e:
+		print "MySQL error %d: %s" %(e.args[0], e.args[1])
+
 
 if __name__ == '__main__':
 	#insert("朱新伟啊啊", "马上吃饭")
-	drop
+	#drop_create_database()
+	drop_create_table()
