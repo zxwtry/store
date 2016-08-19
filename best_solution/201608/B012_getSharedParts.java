c8cb6d766064729344d24ec50b2580f92217b137
zxwtry <zxwtry@qq.com>
2016-08-19 22:46:57 +0800

    添加B012 getSharedParts

---
+++ best_solution/201608/B012_getSharedParts.java
@@ -0,0 +1,11 @@
+public class B012_getSharedParts {
+	public static void main(String[] args) {
+
+	}
+	/*
+	在这里假定list1和list2都是不能够做任何修改的。
+	*/
+	static LinkedList<Node> getSharedParts(LinkedList<Node> list1, LinkedList<Node> list2) {
+		
+	}
+}
