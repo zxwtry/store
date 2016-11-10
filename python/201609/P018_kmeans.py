add2831b0eeea795f24ccbeece1ffa8647a8afa4
zxwtry <zxwtry@qq.com>
2016-11-10 22:06:59 +0800

    添加 python/201609/P018_kmeans.py

---
+++ python/201609/P018_kmeans.py
@@ -0,0 +1,114 @@
+#!/bin/python
+#coding=utf-8
+from numpy import *
+
+def distEclud(vecA, vecB):
+    return sqrt(sum(power(vecA - vecB, 2))) #la.norm(vecA-vecB)
+
+def randCent(dataSet, k):
+    n = shape(dataSet)[1]
+    centroids = mat(zeros((k,n)))#create centroid mat
+    for j in range(n):#create random cluster centers, within bounds of each dimension
+        minJ = min(dataSet[:,j]) 
+        rangeJ = float(max(dataSet[:,j]) - minJ)
+        centroids[:,j] = mat(minJ + rangeJ * random.rand(k,1))
+    return centroids
+    
+def kMeans(dataSet, k, distMeas=distEclud, createCent=randCent):
+    m = shape(dataSet)[0]
+    clusterAssment = mat(zeros((m,2)))#create mat to assign data points 
+                                      #to a centroid, also holds SE of each point
+    centroids = createCent(dataSet, k)
+    clusterChanged = True
+    while clusterChanged:
+        clusterChanged = False
+        for i in range(m):#for each data point assign it to the closest centroid
+            minDist = inf; minIndex = -1
+            for j in range(k):
+                distJI = distMeas(centroids[j,:],dataSet[i,:])
+                if distJI < minDist:
+                    minDist = distJI; minIndex = j
+            if clusterAssment[i,0] != minIndex: clusterChanged = True
+            clusterAssment[i,:] = minIndex,minDist**2
+        print centroids
+        for cent in range(k):#recalculate centroids
+            ptsInClust = dataSet[nonzero(clusterAssment[:,0].A==cent)[0]]#get all the point in this cluster
+            centroids[cent,:] = mean(ptsInClust, axis=0) #assign centroid to mean 
+    return centroids, clusterAssment
+
+def biKmeans(dataSet, k, distMeas=distEclud):
+    m = shape(dataSet)[0]
+    clusterAssment = mat(zeros((m,2)))
+    centroid0 = mean(dataSet, axis=0).tolist()[0]
+    centList =[centroid0] #create a list with one centroid
+    for j in range(m):#calc initial Error
+        clusterAssment[j,1] = distMeas(mat(centroid0), dataSet[j,:])**2
+    while (len(centList) < k):
+        lowestSSE = inf
+        for i in range(len(centList)):
+            ptsInCurrCluster = dataSet[nonzero(clusterAssment[:,0].A==i)[0],:]#get the data points currently in cluster i
+            centroidMat, splitClustAss = kMeans(ptsInCurrCluster, 2, distMeas)
+            sseSplit = sum(splitClustAss[:,1])#compare the SSE to the currrent minimum
+            sseNotSplit = sum(clusterAssment[nonzero(clusterAssment[:,0].A!=i)[0],1])
+            print "sseSplit, and notSplit: ",sseSplit,sseNotSplit
+            if (sseSplit + sseNotSplit) < lowestSSE:
+                bestCentToSplit = i
+                bestNewCents = centroidMat
+                bestClustAss = splitClustAss.copy()
+                lowestSSE = sseSplit + sseNotSplit
+        bestClustAss[nonzero(bestClustAss[:,0].A == 1)[0],0] = len(centList) #change 1 to 3,4, or whatever
+        bestClustAss[nonzero(bestClustAss[:,0].A == 0)[0],0] = bestCentToSplit
+        print 'the bestCentToSplit is: ',bestCentToSplit
+        print 'the len of bestClustAss is: ', len(bestClustAss)
+        centList[bestCentToSplit] = bestNewCents[0,:].tolist()[0]#replace a centroid with two best centroids 
+        centList.append(bestNewCents[1,:].tolist()[0])
+        clusterAssment[nonzero(clusterAssment[:,0].A == bestCentToSplit)[0],:]= bestClustAss#reassign new clusters, and SSE
+    return mat(centList), clusterAssment
+
+def distSLC(vecA, vecB):#Spherical Law of Cosines
+    a = sin(vecA[0,1]*pi/180) * sin(vecB[0,1]*pi/180)
+    b = cos(vecA[0,1]*pi/180) * cos(vecB[0,1]*pi/180) * \
+                      cos(pi * (vecB[0,0]-vecA[0,0]) /180)
+    return arccos(a + b)*6371.0 #pi is imported with numpy
+
+import matplotlib
+import matplotlib.pyplot as plt
+def clusterClubs(numClust=5):
+    datList = []
+    min1=9999
+    min2=9999
+    for line in open('data_format.txt').readlines():
+        lineArr = line.split(' ')
+        datList.append([float(lineArr[1]), float(lineArr[2])])
+        if min1>float(lineArr[1]):
+            min1=float(lineArr[1])
+        if min2>float(lineArr[2]):
+            min2=float(lineArr[2])
+    print "min1:"+str(min1)
+    print "min2:"+str(min2)
+    datMat = mat(datList)
+    myCentroids, clustAssing = biKmeans(datMat, numClust, distMeas=distSLC)
+    print ("Useful")
+    print (myCentroids)
+    m=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    col_origin=["ro","wo","co","go","ko","mo","yo","bo"]
+    col=[]
+    for i in range(numClust):
+        if i <= 7:
+            col.append(col_origin[i])
+        else:
+            col.append(col_origin[random.randint(0,7)])
+    for i in range(len(clustAssing)):
+        a=int(clustAssing[i].getA1()[0])
+        #print int(clustAssing[i].getA1()[1])
+        #print a
+        plt.plot(datList[i][1],datList[i][0],col[a])
+        m[a]=m[a]+1
+        #k = int(clustAssing[i])
+        #m[k] = m[k]+1
+    plt.legend()
+    plt.show()
+    print m
+
+if __name__=="__main__":
+    clusterClubs(6)
