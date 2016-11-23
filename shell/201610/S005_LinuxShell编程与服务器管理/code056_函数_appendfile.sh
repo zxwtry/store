31faf83b46289863824809db24daec51728154cf
zxwtry <zxwtry@qq.com>
2016-11-23 22:24:42 +0800

    添加 shell/201610/S005_LinuxShell编程与服务器管理/code056_ 函数_appendfile.sh

--- shell/201610/S005_LinuxShell编程与服务器管理/code056_函数_appendfile.sh
+++ shell/201610/S005_LinuxShell编程与服务器管理/code056_函数_appendfile.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-#把$2的内容
+#把$2的内容转向附加到$1指定的内容中
+
+appendfile() {
+    echo "$2" >> "$1"
+}
+
+Dir=~/temp
+OutFile="$Dir/a.txt"
+
+#如果目录不存在，则开新目录
+[ -e $Dir ] || mkdir -p $Dir
+
+appendfile $OutFile "函数也可以这样用"
+appendfile $OutFile "这是函数的另外一种用法"
+appendfile $OutFile "Over"
+
