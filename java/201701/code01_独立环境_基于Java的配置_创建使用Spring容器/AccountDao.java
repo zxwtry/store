b2d70e85f923424a1c4ef2c7892bcb30a70d7036
zxwtry <zxwtry@qq.com>
2017-01-11 21:34:47 +0800

    添加 java/201701/code01_独立环境_基于Java的配置_创建使用Spring容器/AccountDao*

---
+++ java/201701/code01_独立环境_基于Java的配置_创建使用Spring容器/AccountDao.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public interface AccountDao {
+	public void insert(Account account);
+	public void update(Account account);
+	public Account find(long accountId);
+	public List<Account> find(String ownerName);
+}
