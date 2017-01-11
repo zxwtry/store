7ceddf24ed2ff50f0081ca5657b2ccb816a733a0
zxwtry <zxwtry@qq.com>
2017-01-11 21:36:08 +0800

    添加 java/201701/code01_独立环境_基于Java的配置_创建使用Spring容器/BeanConfiguration.java

---
+++ java/201701/code01_独立环境_基于Java的配置_创建使用Spring容器/BeanConfiguration.java
@@ -0,0 +1,16 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+	@Bean
+	public AccountService accountService() {
+		AccountService accountService = new AccountServiceImpl(accountDao());
+		return accountService;
+	}
+	@Bean
+	public AccountDao accountDao() {
+		AccountDao accountDao = new AccountDaoImpl();
+		return accountDao;
+	}
+}
