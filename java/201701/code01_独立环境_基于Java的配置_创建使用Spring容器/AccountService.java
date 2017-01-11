b0a024e2d064d914c055b8eb207c7de002e7f0eb
zxwtry <zxwtry@qq.com>
2017-01-11 21:35:38 +0800

    添加 java/201701/code01_独立环境_基于Java的配置_创建使用Spring容器/AccountService*

---
+++ java/201701/code01_独立环境_基于Java的配置_创建使用Spring容器/AccountService.java
@@ -0,0 +1,5 @@
+public interface AccountService {
+	public void transferMoney(long sourceAccountId, long targetAccountId, double amount);
+	public void depositMoney(long accountId, double amount);
+	public Account getAccount(long accountId);
+}
