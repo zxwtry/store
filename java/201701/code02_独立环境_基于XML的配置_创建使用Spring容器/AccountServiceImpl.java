4a0cb1d7944c26e2b35d98e29e5d89c27baca099
zxwtry <zxwtry@qq.com>
2017-01-11 22:22:11 +0800

    添加 java/201701/code02_独立环境_基于XML的配置_创建使用Spring容器/AccountServiceImpl.java

---
+++ java/201701/code02_独立环境_基于XML的配置_创建使用Spring容器/AccountServiceImpl.java
@@ -0,0 +1,37 @@
+package step02XMLBean;
+
+import step01BeanConfigure.Account;
+import step01BeanConfigure.AccountDao;
+import step01BeanConfigure.AccountService;
+
+public class AccountServiceImpl implements AccountService {
+
+	private AccountDao accountDao;
+	
+	public void setAccountDao(AccountDao accountDao) {
+		this.accountDao = accountDao;
+	}
+	
+	@Override
+	public void transferMoney(long sourceAccountId, long targetAccountId, double amount) {
+		Account sourceAccount = accountDao.find(sourceAccountId);
+		Account targetAccount = accountDao.find(targetAccountId);
+		sourceAccount.setBalance(sourceAccount.getBalance() - amount);
+		targetAccount.setBalance(targetAccount.getBalance() + amount);
+		accountDao.update(sourceAccount);
+		accountDao.update(targetAccount);
+	}
+
+	@Override
+	public void depositMoney(long accountId, double amount) {
+		Account account = accountDao.find(accountId);
+		account.setBalance(account.getBalance() + amount);
+		accountDao.update(account);
+	}
+
+	@Override
+	public Account getAccount(long accountId) {
+		return accountDao.find(accountId);
+	}
+
+}
