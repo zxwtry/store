209b4ef9d6ad3b273dfc7e2e9d2c482a8441f06a
zxwtry <zxwtry@qq.com>
2017-01-11 21:32:35 +0800

    添加 java/201701/code01_独立环境_基于Java的配置_创建使用Spring容器/Account.java 到 Account域类

---
+++ java/201701/code01_独立环境_基于Java的配置_创建使用Spring容器/Account.java
@@ -0,0 +1,23 @@
+public class Account {
+	private long accountId;
+	private double balance;
+	private String ownerName;
+	public long getAccountId() {
+		return accountId;
+	}
+	public void setAccountId(long accountId) {
+		this.accountId = accountId;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public String getOwnerName() {
+		return ownerName;
+	}
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
+	}
+}
