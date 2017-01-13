e4ac9a5bfeb8bfaa5bbb4307b9ddd38694dfcba1
zxwtry <zxwtry@qq.com>
2017-01-13 11:30:39 +0800

    添加 java/201701/code05_独立环境_基于Java的配置_重写Bean定义_创建使用Spring容器/FunSon.java

---
+++ java/201701/code05_独立环境_基于Java的配置_重写Bean定义_创建使用Spring容器/Fun.java
@@ -0,0 +1,13 @@
+package step05OverrideBeanDefinition;
+
+public class Fun {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ java/201701/code05_独立环境_基于Java的配置_重写Bean定义_创建使用Spring容器/FunSon.java
@@ -0,0 +1,4 @@
+package step05OverrideBeanDefinition;
+
+public class FunSon extends Fun {
+}
