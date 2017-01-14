19557833733ffee013df543cfa4ace1ad87a1925
zxwtry <zxwtry@qq.com>
2017-01-14 20:30:40 +0800

    添加 java/201701/code06_独立环境_基于Java的配置_自动装配_创建使用Spring容器/Main.java

---
+++ java/201701/code06_独立环境_基于Java的配置_自动装配_创建使用Spring容器/Main.java
@@ -0,0 +1,24 @@
+package step06JavaAutowiring;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(Ch2BeanConfiguration.class);
+		AccountService accountService = applicationContext.getBean("accountService", AccountService.class);
+
+		System.out.println("Before money transfer");
+		System.out.println("Account 1 balance :" + accountService.getAccount(1).getBalance());
+		System.out.println("Account 2 balance :" + accountService.getAccount(2).getBalance());		
+		
+		accountService.transferMoney(1, 2, 5.0);
+		
+		System.out.println("After money transfer");
+		System.out.println("Account 1 balance :" + accountService.getAccount(1).getBalance());
+		System.out.println("Account 2 balance :" + accountService.getAccount(2).getBalance());
+		
+		applicationContext.close();
+	}
+
+}
