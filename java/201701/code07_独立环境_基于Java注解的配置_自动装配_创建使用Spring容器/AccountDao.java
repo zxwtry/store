f05113803a007dd7c3434dbee633b524380d5cfd
zxwtry <zxwtry@qq.com>
2017-01-15 21:34:57 +0800

    添加 java/201701/code07_独立环境_基于Java注解的配置_自动装配_创建使用Spring容器/AccountDao.java

---
+++ java/201701/code07_独立环境_基于Java注解的配置_自动装配_创建使用Spring容器/AccountDao.java
@@ -0,0 +1,14 @@
+package step07AnnotationAutowired;
+
+import java.util.List;
+
+public interface AccountDao {
+    public void insert(Account account);
+    public void update(Account account);
+    public void update(List<Account> accounts);
+    public void delete(long accountId);
+    public Account find(long accountId);
+    public List<Account> find(List<Long> accountIds);
+    public List<Account> find(String ownerName);
+    public List<Account> find(boolean locked);
+}
