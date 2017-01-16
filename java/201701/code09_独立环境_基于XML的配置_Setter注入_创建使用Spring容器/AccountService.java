35bc8b9769254009982703197dbaa7399b9986d2
zxwtry <zxwtry@qq.com>
2017-01-16 17:06:50 +0800

    添加 java/201701/code09_独立环境_基于XML的配置_Setter注入_创建使用Spring容器/AccountService.java

---
+++ java/201701/code09_独立环境_基于XML的配置_Setter注入_创建使用Spring容器/AccountService.java
@@ -0,0 +1,8 @@
+package step09SetterInjection;
+
+
+public interface AccountService {
+	public void transferMoney(long sourceAccountId, long targetAccountId, double amount);
+	public void depositMoney(long accountId, double amount) throws Exception;
+	public Account getAccount(long accountId);
+}
