a7a0b6057f44bcb961def4564766cd24ceb711f5
zxwtry <zxwtry@qq.com>
2017-01-16 15:52:38 +0800

    添加 java/201701/code08_独立环境_基于XML的配置_自动装配_创建使用Spring容器/Main.java

---
+++ java/201701/code08_独立环境_基于XML的配置_自动装配_创建使用Spring容器/Main.java
@@ -0,0 +1,25 @@
+package step08XMLAnnotationAutowired;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("step08XMLAnnotationAutowired/ch2-beans.xml");
+		AccountService accountService = applicationContext.getBean("accountService", AccountService.class);
+
+		System.out.println("Before money transfer ");
+		System.out.println("Account 1 balance : " + accountService.getAccount(1).getBalance());
+		System.out.println("Account 2 balance : " + accountService.getAccount(2).getBalance());		
+		
+		accountService.transferMoney(1, 2, 5.0);
+		
+		System.out.println("After money transfer ");
+		System.out.println("Account 1 balance : " + accountService.getAccount(1).getBalance());
+		System.out.println("Account 2 balance : " + accountService.getAccount(2).getBalance());
+
+		applicationContext.close();
+		
+	}
+
+}
