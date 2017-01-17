a17b3e49be7cc032b5fa3fd5cc139c568e6784db
zxwtry <zxwtry@qq.com>
2017-01-17 19:53:35 +0800

    添加 java/201701/code11_独立环境_InstantiationMethods_创建使用Spring容器/Main.java

---
+++ java/201701/code11_独立环境_InstantiationMethods_创建使用Spring容器/Main.java
@@ -0,0 +1,23 @@
+package step11InstantiationMethods;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:/applicationContext.xml");
+		applicationContext.registerShutdownHook();
+		Foo foo3 = applicationContext.getBean("foo3", Foo.class);
+		Foo foo4 = applicationContext.getBean("foo4", Foo.class);
+		
+		System.out.println(foo3.getName());
+		System.out.println(foo4.getName());
+		
+		applicationContext.close();
+		
+	}
+
+}
