e2ac46ed75d901366283f0a0777ef42fdcc69633
zxwtry <zxwtry@qq.com>
2017-01-17 19:46:55 +0800

    添加 java/201701/code10_独立环境_ConfiguringEnvironment_创建使用Spring容器/Foo.java

---
+++ java/201701/code10_独立环境_ConfiguringEnvironment_创建使用Spring容器/Foo.java
@@ -0,0 +1,15 @@
+package step10ConfiguringEnvironment;
+
+public class Foo {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
