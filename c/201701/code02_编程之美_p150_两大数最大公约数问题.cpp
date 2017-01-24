47199d6fe130de2e25c948932e51343bd05c5097
zxwtry <zxwtry@qq.com>
2017-01-24 23:13:36 +0800

    添加 c/201701/code02_编程之美_p150_两大数最大公约数问题.cpp

--- c/201701/code02_编程之美_p150_两大数最大公约数问题.cpp
+++ c/201701/code02_编程之美_p150_两大数最大公约数问题.cpp
@@ -6,8 +6,16 @@ LL gcd1(LL x, LL y) {
     return y == 0l ? x : gcd1(y, x % y);
 }
 
+//考虑到大数相除运算量很大，改成相减
+LL gcd2(LL x, LL y) {
+    if (x < y)
+        return gcd2(y, x);
+    if (y == 0)
+        return x;
+    return gcd2(x - y, y);
+}
 
 int main() {
     LL x = 16, y = 24;
-    printf("%lld\r\n", gcd1(x, y));
+    printf("%lld\r\n", gcd2(x, y));
 }
