614e16bbab88f29de93a93156b8fc4f408a20c5d
zxwtry <zxwtry@qq.com>
2017-01-25 22:19:37 +0800

    添加 c/common_header_cpp.cpp

---
+++ c/common_header_cpp.cpp
@@ -0,0 +1,99 @@
+#include <functional>
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <numeric>
+#include <cstring>
+#include <climits>
+#include <cassert>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <bitset>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <ctime>
+#include <list>
+#include <set>
+#include <map>
+
+using namespace std;
+
+#define FOR(i, a, b) for (int i=int(a);i<int(b);++i)
+#define DWN(i, b, a) for (int i=int(b-1);i>=int(a);--i)
+#define REP_1(i, n) for (int i=1;i<=int(n);++i)
+#define FOR_1(i, a, b) for (int i=int(a);i<=int(b);++i)
+#define DWN_1(i, b, a) for (int i=int(b);i>=int(a);--i)
+#define REP_C(i, n) for (int n____=int(n),i=0;i<n____;++i)
+#define FOR_C(i, a, b) for (int b____=int(b),i=a;i<b____;++i)
+#define DWN_C(i, b, a) for (int a____=int(a),i=b-1;i>=a____;--i)
+#define REP_N(i, n) for (i=0;i<int(n);++i)
+#define FOR_N(i, a, b) for (i=int(a);i<int(b);++i)
+#define DWN_N(i, b, a) for (i=int(b-1);i>=int(a);--i)
+#define REP_1_C(i, n) for (int n____=int(n),i=1;i<=n____;++i)
+#define FOR_1_C(i, a, b) for (int b____=int(b),i=a;i<=b____;++i)
+#define DWN_1_C(i, b, a) for (int a____=int(a),i=b;i>=a____;--i)
+#define REP_1_N(i, n) for (i=1;i<=int(n);++i)
+#define FOR_1_N(i, a, b) for (i=int(a);i<=int(b);++i)
+#define DWN_1_N(i, b, a) for (i=int(b);i>=int(a);--i)
+#define REP_C_N(i, n) for (int n____=(i=0,int(n));i<n____;++i)
+#define FOR_C_N(i, a, b) for (int b____=(i=0,int(b);i<b____;++i)
+#define DWN_C_N(i, b, a) for (int a____=(i=b-1,int(a));i>=a____;--i)
+#define REP_1_C_N(i, n) for (int n____=(i=1,int(n));i<=n____;++i)
+#define FOR_1_C_N(i, a, b) for (int b____=(i=1,int(b);i<=b____;++i)
+#define DWN_1_C_N(i, b, a) for (int a____=(i=b,int(a));i>=a____;--i)
+
+#define ECH(it, A) for (__typeof(A.begin()) it=A.begin(); it != A.end(); ++it)
+#define REP_S(i, str) for (char*i=str;*i;++i)
+#define REP_L(i, hd, nxt) for (int i=hd;i;i=nxt[i])
+#define REP_G(i, u) REP_L(i,hd[u],suc)
+#define DO(n) for ( int ____n ## __line__ = n; ____n ## __line__ -- ; )
+#define REP_2(i, j, n, m) REP(i, n) REP(j, m)
+#define REP_2_1(i, j, n, m) REP_1(i, n) REP_1(j, m)
+#define REP_3(i, j, k, n, m, l) REP(i, n) REP(j, m) REP(k, l)
+#define REP_3_1(i, j, k, n, m, l) REP_1(i, n) REP_1(j, m) REP_1(k, l)
+#define REP_4(i, j, k, ii, n, m, l, nn) REP(i, n) REP(j, m) REP(k, l) REP(ii, nn)
+#define REP_4_1(i, j, k, ii, n, m, l, nn) REP_1(i, n) REP_1(j, m) REP_1(k, l) REP_1(ii, nn)
+
+
+#define LOCALLL(A) A.begin(), A.end()
+#define LLA(A) A.rbegin(), A.rend()
+#define CPY(A, B) memcpy(A, B, sizeof(A))
+#define INS(A, P, B) A.insert(A.begin() + P, B)
+#define ERS(A, P) A.erase(A.begin() + P)
+#define BSC(A, x) (lower_bound(ALL(A), x) - A.begin())
+#define CTN(T, x) (T.find(x) != T.end())
+#define SZ(A) int((A).size())
+#define PB push_back
+#define MP(A, B) make_pair(A, B)
+#define PTT pair<T, T>
+#define fi first
+#define se second
+
+typedef long long LL;
+typedef double DB;
+typedef unsigned UINT;
+typedef unsigned long long ULL;
+
+typedef vector<int> VI;
+typedef vector<char> VC;
+typedef vector<string> VS;
+typedef vector<LL> VL;
+typedef vector<DB> VF;
+typedef set<int> SI;
+typedef set<string> SS;
+typedef map<int, int> MII;
+typedef map<string, int> MSI;
+typedef pair<int, int> PII;
+typedef pair<LL, LL> PLL;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef vector<VII> VVII;
+
+int main() {
+    VL vl;
+    printf("%d\r\n", SZ(vl));
+}
