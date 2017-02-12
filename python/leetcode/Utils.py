a179d09f8ed39cfd7724ee1dc4fd506138d54acd
zxwtry <zxwtry@qq.com>
2017-02-12 20:22:19 +0800

    添加 leetcode/Utils.py

---
+++ python/leetcode/Utils.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+
+'''
+@author:     zxwtry
+@email:      zxwtry@qq.com
+@date:       2017年2月12日
+@details:    
+'''
+
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
