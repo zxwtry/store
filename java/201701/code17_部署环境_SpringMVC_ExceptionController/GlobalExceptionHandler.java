af35b139a3c5797c9be96c43c38bcd8a22c3eb31
zxwtry <zxwtry@qq.com>
2017-03-29 22:32:35 +0800

    add java/201701/code17_部署环境_SpringMVC_ExceptionController/GlobalExceptionHandler.java

---
+++ java/201701/code17_部署环境_SpringMVC_ExceptionController/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package step17ExceptionController;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handlerException() {
+        return new ModelAndView("errorGlobal");
+    }
+}
