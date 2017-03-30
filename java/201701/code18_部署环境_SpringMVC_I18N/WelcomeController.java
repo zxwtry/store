987c71d1845bba45d35a0e090e58b59df2fcbf25
zxwtry <zxwtry@qq.com>
2017-03-30 20:35:28 +0800

    add java/201701/code18_部署环境_SpringMVC_I18N/WelcomeController.java

---
+++ java/201701/code18_部署环境_SpringMVC_I18N/WelcomeController.java
@@ -0,0 +1,13 @@
+package step18I18N;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WelcomeController {
+    @RequestMapping(value="/welcome")
+    public ModelAndView welcome() {
+        return new ModelAndView("welcome");
+    }
+}
