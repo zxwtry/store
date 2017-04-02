a16cd8c1bbde0c02c7e38e79c0870197b4bb5de1
zxwtry <zxwtry@qq.com>
2017-04-02 12:51:17 +0800

    add java/201701/code24_独立环境_RunningQueryUsingPreparedStatements/Main.java

--- java/201701/code24_独立环境_RunningQueryUsingPreparedStatements/Main.java
+++ java/201701/code24_独立环境_RunningQueryUsingPreparedStatements/Main.java
@@ -1,17 +1,43 @@
 package step24RunningQueryUsingPreparedStatements;
 
-import java.sql.Connection;
-
-import javax.sql.DataSource;
+import java.sql.SQLException;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
-    public static void main(String[] args) {
+    //6     data：pre 1000 times time is 5644
+    //162   data：pre 1000 times time is 6233
+    static void pre1000times() {
+        long time = System.currentTimeMillis();
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(ConfigurationPreparedStatements.class);
-        DataSource dataSource = applicationContext.getBean(DataSource.class);
-        Connection connection = dataSource.getConnection();
-        
+        AccountDao accountDao = applicationContext.getBean(AccountDao.class);
+        for (int i = 0; i < 1000; i ++)
+            accountDao.find(false);
         applicationContext.close();
+        System.out.printf("pre 1000 times time is %d\r\n", (System.currentTimeMillis() - time));
+    }
+    //6     data：no pre 1000 times time is 5834
+    //162   data：no pre 1000 times time is 6338
+    static void noPre1000times() {
+        long time = System.currentTimeMillis();
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(ConfigurationPreparedStatements.class);
+        AccountDao accountDao = applicationContext.getBean(AccountDao.class);
+        for (int i = 0; i < 1000; i ++)
+            accountDao.findNoPre(false);
+        applicationContext.close();
+        System.out.printf("no pre 1000 times time is %d\r\n", (System.currentTimeMillis() - time));
+    }
+    //162   data: book 1000 times time is 6196
+    static void book1000times() {
+        long time = System.currentTimeMillis();
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(ConfigurationPreparedStatements.class);
+        AccountDao accountDao = applicationContext.getBean(AccountDao.class);
+        for (int i = 0; i < 1000; i ++)
+            accountDao.findBook(false);
+        applicationContext.close();
+        System.out.printf("book 1000 times time is %d\r\n", (System.currentTimeMillis() - time));
+    }
+    public static void main(String[] args) throws SQLException {
+        book1000times();
     }
 }
