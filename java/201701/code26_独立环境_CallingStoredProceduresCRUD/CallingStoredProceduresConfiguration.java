53c5f49d2871a233dca0d146944ba57f0c79de6b
zxwtry <zxwtry@qq.com>
2017-04-04 19:43:35 +0800

    add java/201701/code26_独立环境_CallingStoredProceduresCRUD/CallingStoredProceduresConfiguration.java

---
+++ java/201701/code26_独立环境_CallingStoredProceduresCRUD/CallingStoredProceduresConfiguration.java
@@ -0,0 +1,28 @@
+package step26CallingStoredProceduresCRUD;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class CallingStoredProceduresConfiguration {
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://192.168.139.130:3306/test_springmvc");
+        dataSource.setUsername("root");
+        dataSource.setPassword("12356");
+        return dataSource;
+    }
+    
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(dataSource());
+        return jdbcTemplate;
+    }
+}
