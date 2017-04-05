2286a04fe79219f53d13a24be6cec77acf12b6e7
zxwtry <zxwtry@qq.com>
2017-04-05 21:54:32 +0800

    add java/201701/code27_独立环境_PerformingBatchOperations/BatchConfiguration.java

---
+++ java/201701/code27_独立环境_PerformingBatchOperations/BatchConfiguration.java
@@ -0,0 +1,36 @@
+package step27PerformingBatchOperations;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class BatchConfiguration {
+    @Bean
+    public AccountDao accountDao() {
+        AccountDaoImpl accountDao = new AccountDaoImpl();
+        accountDao.setJdbcTemplate(jdbcTemplate());
+        return accountDao;
+    }
+    
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(dataSource());
+        return jdbcTemplate;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://192.168.139.130:3306/test_springmvc");
+        dataSource.setUsername("root");
+        dataSource.setPassword("12356");
+        return dataSource;
+    }
+    
+}
