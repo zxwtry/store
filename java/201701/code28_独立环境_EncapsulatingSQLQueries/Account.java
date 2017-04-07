23946a1587b5de47dd4a99eb5ee8affda9d61e74
zxwtry <zxwtry@qq.com>
2017-04-07 22:07:14 +0800

    add java/201701/code28_独立环境_EncapsulatingSQLQueries/Account.java

--- java/201701/code28_独立环境_EncapsulatingSQLQueries/Account.java
+++ java/201701/code28_独立环境_EncapsulatingSQLQueries/Account.java
@@ -1,5 +1,6 @@
-package com.wiley.beginningspring.ch4;
+package step28EncapsulatingSQLQueries;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Account {
@@ -8,38 +9,39 @@ public class Account {
     private double balance;
     private Date accessTime;
     private boolean locked;
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getOwnerName() {
-		return ownerName;
-	}
-	public void setOwnerName(String ownerName) {
-		this.ownerName = ownerName;
-	}
-	public double getBalance() {
-		return balance;
-	}
-	public void setBalance(double balance) {
-		this.balance = balance;
-	}
-	public Date getAccessTime() {
-		return accessTime;
-	}
-	public void setAccessTime(Date accessTime) {
-		this.accessTime = accessTime;
-	}
-	public boolean isLocked() {
-		return locked;
-	}
-	public void setLocked(boolean locked) {
-		this.locked = locked;
-	}
-    
-    //getters & setters...
-    
-    
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getOwnerName() {
+        return ownerName;
+    }
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    public Date getAccessTime() {
+        return accessTime;
+    }
+    public void setAccessTime(Date accessTime) {
+        this.accessTime = accessTime;
+    }
+    public boolean isLocked() {
+        return locked;
+    }
+    public void setLocked(boolean locked) {
+        this.locked = locked;
+    }
+    @Override
+    public String toString() {
+        return String.format("Account id is %d\r\nAccount ownerName is %s\r\nAccount balance is %f\r\nAccount accessTime is %s\r\nAccount locked is %b\r\n", 
+                id, ownerName, balance, new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(accessTime), locked);
+    }
 }
