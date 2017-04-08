503c5126dac98324a1f98ca510637d9e9b60ae06
zxwtry <zxwtry@qq.com>
2017-04-08 22:14:01 +0800

    add java/201701/code29_独立环境_EncapsulatingSQLDML/DMLConfiguration.java

---
+++ java/201701/code29_独立环境_EncapsulatingSQLDML/DMLConfiguration.java
@@ -0,0 +1,43 @@
+package step29EncapsulatingSQLDML;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DMLConfiguration {
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://192.168.139.130:3306/test_springmvc");
+        dataSource.setUsername("root");
+        dataSource.setPassword("12356");
+        return dataSource;
+    }
+    @Bean
+    public AccountDelete accountDelete() {
+        AccountDelete accountDelete = new AccountDelete(dataSource());
+        return accountDelete;
+    }
+    @Bean
+    public AccountInsert accountInsert() {
+        AccountInsert accountInsert = new AccountInsert(dataSource());
+        return accountInsert;
+    }
+    @Bean
+    public AccountUpdate accountUpdate() {
+        AccountUpdate accountUpdate = new AccountUpdate(dataSource());
+        return accountUpdate;
+    }
+    @Bean
+    public AccountDao accountDao() {
+        AccountDaoImpl accountDao = new AccountDaoImpl();
+        accountDao.setAccountDelete(accountDelete());
+        accountDao.setAccountInsert(accountInsert());
+        accountDao.setAccountUpdate(accountUpdate());
+        return accountDao;
+    }
+}
