4725e78b0a1acb370b87fa51f2e5d1b2a569ec64
zxwtry <zxwtry@qq.com>
2017-04-09 22:29:39 +0800

    modify java/201701/code26_独立环境_CallingStoredProceduresCRUD/Main.java

--- java/201701/code26_独立环境_CallingStoredProceduresCRUD/Main.java
+++ java/201701/code26_独立环境_CallingStoredProceduresCRUD/Main.java
@@ -1,38 +1,29 @@
 package step26CallingStoredProceduresCRUD;
 
-import java.sql.Types;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.SingleColumnRowMapper;
-import org.springframework.jdbc.core.SqlParameter;
-import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 
 public class Main {
+    
     public static void main(String[] args) {
+        solve();
+//        solveAccount();
+    }
+
+//    static void solveAccount() {
+//        AnnotationConfigApplicationContext applicationContext = new 
+//                AnnotationConfigApplicationContext(CallingStoredProceduresConfiguration.class);
+//        AccountDao accountDao = applicationContext.getBean(AccountDao.class);
+//        Account account = accountDao.findAccountById(56);
+//        System.out.println(account);
+//        applicationContext.close();
+//    }
+
+    static void solve() {
         AnnotationConfigApplicationContext applicationContext = new 
                 AnnotationConfigApplicationContext(CallingStoredProceduresConfiguration.class);
-        JdbcTemplate jdbcTemplate = applicationContext.getBean(JdbcTemplate.class);
-        SimpleJdbcCall simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate);
-        simpleJdbcCall
-            .withProcedureName("concat")
-            .withoutProcedureColumnMetaDataAccess()
-            .declareParameters(
-                    new SqlParameter("owner_name", Types.VARCHAR))
-            .returningResultSet("result", new SingleColumnRowMapper<>(String.class));
-        simpleJdbcCall.compile();
-        Map<String, Object> paramMap = new HashMap<String, Object>();
-        paramMap.put("owner_name", "Bear Junior");
-        Map<String, Object> resultMap = simpleJdbcCall.execute(paramMap);
-        
-        @SuppressWarnings("unchecked")
-        List<String> resultList = (List<String>) resultMap.get("result");
-        for (String value : resultList)
-            System.out.println(value);
-        
+        AccountDao accountDao = applicationContext.getBean(AccountDao.class);
+        double balance = accountDao.findBalanceById(56);
+        System.out.println(balance);
         applicationContext.close();
     }
 }
