c977fd65840a46f0fcb604552cba4c8e37eb7482
zxwtry <zxwtry@qq.com>
2017-04-09 22:29:48 +0800

    modify java/201701/code26_独立环境_CallingStoredProceduresCRUD/CallingStoredProceduresConfiguration.java

--- java/201701/code26_独立环境_CallingStoredProceduresCRUD/CallingStoredProceduresConfiguration.java
+++ java/201701/code26_独立环境_CallingStoredProceduresCRUD/CallingStoredProceduresConfiguration.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
@@ -18,11 +19,17 @@ public class CallingStoredProceduresConfiguration {
         dataSource.setPassword("12356");
         return dataSource;
     }
-    
     @Bean
     public JdbcTemplate jdbcTemplate() {
         JdbcTemplate jdbcTemplate = new JdbcTemplate();
         jdbcTemplate.setDataSource(dataSource());
         return jdbcTemplate;
+    }
+    @Bean
+    public AccountDao accountDao() {
+        AccountDaoImpl accountDao = new AccountDaoImpl();
+        SimpleJdbcCall simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate());
+        accountDao.setSimpleJdbcCall(simpleJdbcCall);
+        return accountDao;
     }
 }
