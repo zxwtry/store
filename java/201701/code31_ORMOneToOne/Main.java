f0bfe657ce59c3770b1b7aeadd36255f23bc5ad8
zxwtry <zxwtry@qq.com>
2017-04-13 22:20:23 +0800

    modify java/201701/code31_ORMOneToOne/Main.java

--- java/201701/code31_ORMOneToOne/Main.java
+++ java/201701/code31_ORMOneToOne/Main.java
@@ -8,24 +8,45 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
 	public static void main(String[] args) {
-	    AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
-				ORMConfiguration.class);
-		EntityManagerFactory entityManagerFactory = applicationContext
-				.getBean(EntityManagerFactory.class);
-		EntityManager entityManager = entityManagerFactory
-				.createEntityManager();
-		EntityTransaction transaction = entityManager.getTransaction();
-		transaction.begin();
+	    sovleSingleMapAccount();
+	}
+	static void solve1() {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+                ORMConfiguration.class);
+        EntityManagerFactory entityManagerFactory = applicationContext
+                .getBean(EntityManagerFactory.class);
+        EntityManager entityManager = entityManagerFactory
+                .createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
 
-		Student student = new Student();
-		student.setFirstName("John");
-		student.setLastName("Smith");
+        Student student = new Student();
+        student.setFirstName("John");
+        student.setLastName("Smith");
 
-		entityManager.persist(student);
-		transaction.commit();
-		entityManager.close();
-		
-		applicationContext.close();
+        entityManager.persist(student);
+        transaction.commit();
+        entityManager.close();
+        entityManagerFactory.close();
+        applicationContext.close();
+	}
+	
+	static void sovleSingleMapAccount() {
+	    AnnotationConfigApplicationContext applicationContext = 
+	            new AnnotationConfigApplicationContext(SingleMapConfiguration.class);
+	    EntityManagerFactory entityManagerFactory = 
+	            applicationContext.getBean(EntityManagerFactory.class);
+	    EntityManager entityManager = entityManagerFactory
+	            .createEntityManager();
+	    EntityTransaction entityTransaction = entityManager.getTransaction();
+	    entityTransaction.begin();
+	    SingleMapAccount account = new SingleMapAccount();
+	    account.setName("Bear Junior");
+	    entityManager.persist(account);
+	    entityTransaction.commit();
+//	    entityManager.close();;
+//	    entityManagerFactory.close();
+	    applicationContext.close();
 	}
 
 }
