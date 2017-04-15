058bf0722ab67879a4c1478913bed8577525c1eb
zxwtry <zxwtry@qq.com>
2017-04-15 22:32:09 +0800

    add java/201701/code31_ORMOneToOne/Main.java

--- java/201701/code31_ORMOneToOne/Main.java
+++ java/201701/code31_ORMOneToOne/Main.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
 	public static void main(String[] args) {
-	    sovleSingleMapAccount();
+	    solveOneToMany();
 	}
 	static void solve1() {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
@@ -27,7 +27,6 @@ public class Main {
         entityManager.persist(student);
         transaction.commit();
         entityManager.close();
-        entityManagerFactory.close();
         applicationContext.close();
 	}
 	
@@ -44,9 +43,72 @@ public class Main {
 	    account.setName("Bear Junior");
 	    entityManager.persist(account);
 	    entityTransaction.commit();
-//	    entityManager.close();;
-//	    entityManagerFactory.close();
+	    entityManager.close();;
 	    applicationContext.close();
 	}
-
+	
+	static void solveOntToOne() {
+	    AnnotationConfigApplicationContext applicationContext = 
+	            new AnnotationConfigApplicationContext(OneToOneConfiguration.class);
+	    EntityManagerFactory entityManagerFactory = 
+	            applicationContext.getBean(EntityManagerFactory.class);
+	    EntityManager entityManager = entityManagerFactory
+	            .createEntityManager();
+	    EntityTransaction entityTransaction = entityManager.getTransaction();
+	    entityTransaction.begin();
+	    OneToOneCitizen citizen = new OneToOneCitizen();
+	    OneToOneIDCard idCard = new OneToOneIDCard();
+	    entityManager.persist(idCard);
+	    citizen.setIdCard(idCard);
+	    citizen.setName("my_name");
+	    entityManager.persist(citizen);
+	    entityTransaction.commit();
+	    applicationContext.close();
+	}
+	
+	static void solveManyToOne() {
+	    AnnotationConfigApplicationContext applicationContext = 
+	            new AnnotationConfigApplicationContext(ManyToOneConfiguration.class);
+	    EntityManagerFactory entityManagerFactory = 
+	            applicationContext.getBean(EntityManagerFactory.class);
+	    EntityManager entityManager = entityManagerFactory.createEntityManager();
+	    EntityTransaction entityTransaction = entityManager.getTransaction();
+	    entityTransaction.begin();
+	    ManyToOneEmployee employee1 = new ManyToOneEmployee();
+	    ManyToOneEmployee employee2 = new ManyToOneEmployee();
+	    ManyToOneEmployee employee3 = new ManyToOneEmployee();
+	    ManyToOneEmployer employer1 = new ManyToOneEmployer();
+	    employee1.setEmployer(employer1);
+	    employee2.setEmployer(employer1);
+	    employee3.setEmployer(employer1);
+	    entityManager.persist(employer1);
+	    entityManager.persist(employee1);
+	    entityManager.persist(employee2);
+	    entityManager.persist(employee3);
+	    entityTransaction.commit();
+	    applicationContext.close();
+	}
+	
+	static void solveOneToMany() {
+	    AnnotationConfigApplicationContext applicationContext = 
+	            new AnnotationConfigApplicationContext(OneToOneConfiguration.class);
+	    EntityManagerFactory entityManagerFactory = 
+	            applicationContext.getBean(EntityManagerFactory.class);
+	    EntityManager entityManager = entityManagerFactory.createEntityManager();
+	    EntityTransaction entityTransaction = entityManager.getTransaction();
+	    entityTransaction.begin();
+	    OneToManyEmployee employee1 = new OneToManyEmployee();
+	    OneToManyEmployee employee2 = new OneToManyEmployee();
+	    OneToManyEmployee employee3 = new OneToManyEmployee();
+	    OneToManyEmployer employer = new OneToManyEmployer();
+	    employer.getEmployees().add(employee1);
+	    employer.getEmployees().add(employee2);
+	    employer.getEmployees().add(employee3);
+	    entityManager.persist(employee1);
+	    entityManager.persist(employee2);
+	    entityManager.persist(employee3);
+	    entityManager.persist(employer);
+	    entityTransaction.commit();
+	    applicationContext.close();
+	}
 }
