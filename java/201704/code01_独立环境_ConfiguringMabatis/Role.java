79727e0a2e2363fb2b885679e1f61601d472dd4d
zxwtry <zxwtry@qq.com>
2017-04-17 19:57:20 +0800

    add java/201704/code01_独立环境_ConfiguringMabatis/Role.java

---
+++ java/201704/code01_独立环境_ConfiguringMabatis/Role.java
@@ -0,0 +1,26 @@
+package code01_ConfiguringMabatis.po;
+
+public class Role {
+    private int id;
+    private String roleName;
+    private String note;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getRoleName() {
+        return roleName;
+    }
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+    public String getNote() {
+        return note;
+    }
+    public void setNote(String note) {
+        this.note = note;
+    }
+    
+}
