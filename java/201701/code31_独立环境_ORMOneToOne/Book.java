3f0f2bb3e9a293660f3d1dbfff07ad6d1ac59828
zxwtry <zxwtry@qq.com>
2017-04-26 22:34:58 +0800

    add java/201701/code31_独立环境_ORMOneToOne/Book.java

---
+++ java/201701/code31_独立环境_ORMOneToOne/Book.java
@@ -0,0 +1,26 @@
+package step31ORMOneToOne;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
