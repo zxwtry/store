1d586c4731e9c126331038995626985edc5953bf
zxwtry <zxwtry@qq.com>
2017-08-05 08:57:00 +0800

    add java/201708/HttpFakeError.java

---
+++ java/201708/HttpFakeError.java
@@ -0,0 +1,19 @@
+
+public class HttpFakeError {
+    public static void main(String[] args) {
+        int a = 0;
+        int i = 0;
+        
+        Http://www.zxwtry.com
+            for (; i < 10; i ++) {
+                if (i == 9) break Http;
+                if (i % 2 == 0) {
+                    continue Http;
+                } else {
+                    a ++;
+                }
+            }
+        System.out.println(a);
+        
+    }
+}
